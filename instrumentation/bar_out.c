d9afa129a06896062d419e21815a1b1b346c81fe
Ankit Goyal <ankit3goyal@gmail.com>
2014-02-08 15:42:00 -0600

    modifying sample file

--- instrumentation/bar_out.c
+++ instrumentation/bar_out.c
@@ -3,9 +3,9 @@
 
 #include<stdio.h>
 
-void foo(int b[100], int a[100]){
+void foo(int b[10], int a[10]){
     int i;
-    for(i=99; i > 0; i--){ 
+    for(i=9; i > 0; i--){ 
         b[i] = 12;
         if(i-1 > 1)
             a[i] = 21 + b[i-1];
@@ -14,10 +14,10 @@ void foo(int b[100], int a[100]){
 }
 
 int main(void){
-    int a[100];
-    printf("varinfo:a:100:%08lx\n",(void *) &a);
-    int b[100];
-    printf("varinfo:b:100:%08lx\n",(void *) &b);
+    int a[10];
+    printf("varinfo:a:10:%08lx\n",(void *) &a);
+    int b[10];
+    printf("varinfo:b:10:%08lx\n",(void *) &b);
     a[2] = 23;
     printf("%d", a[2]);
     
