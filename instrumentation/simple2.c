18d9b4c4d68108b1292379237739d1919383b8d6
Ankit Goyal <ankit3goyal@gmail.com>
2014-02-09 23:42:58 -0600

    adding simple2.md

---
+++ instrumentation/simple2.c
@@ -0,0 +1,28 @@
+#define L_AND(a, b) a && b
+#define L_OR(a, b) a || b
+
+#include<stdio.h>
+
+void foo(int b[10], int a[10]){
+    int i;
+    for(i=9; i > 0; i--){ 
+        a[i] = 21 + b[i];
+    }
+    b[3]  = 23;
+}
+
+int main(void){
+    int a[10];
+    printf("varinfo:a:10:%08lx\n",(void *) &a);
+    int b[10];
+    printf("varinfo:b:10:%08lx\n",(void *) &b);
+    a[2] = 23;
+    printf("%d", a[2]);
+    
+    fprintf(stderr, "\nbeforemethodcall:foo\n");
+    foo(a, b);
+    fprintf(stderr, "\naftermethodcall:foo\n");
+    
+
+    return 0;
+}
