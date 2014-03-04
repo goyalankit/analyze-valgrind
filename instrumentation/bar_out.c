34f01cfe0acdc1ca7f0201a8bf7249c79a791c56
Ankit Goyal <ankit3goyal@gmail.com>
2014-03-04 17:51:32 -0600

    makefile to select config path based on machine

--- instrumentation/bar_out.c
+++ instrumentation/bar_out.c
@@ -3,28 +3,25 @@
 
 #include<stdio.h>
 
-void foo(int b[10], int a[10]){
+void foo(int b[100], int a[100]){
     int i;
-    for(i=9; i > 0; i--){ 
+    for(i=99; i > 0; i--){ 
         b[i] = 12;
         if(i-1 > 1)
             a[i] = 21 + b[i-1];
     }
     b[3]  = 23;
+    printf("hello %d",b[2]);
 }
 
 int main(void){
-    int a[10];
-    printf("varinfo:a:10:%08lx\n",(void *) &a);
-    int b[10];
-    printf("varinfo:b:10:%08lx\n",(void *) &b);
+    int a[100];
+    printf("varinfo:a:100:%08lx:%lu\n",(void *) &a, sizeof(a[0]));
+    int b[100];
+    printf("varinfo:b:100:%08lx:%lu\n",(void *) &b, sizeof(b[0]));
     a[2] = 23;
     printf("%d", a[2]);
-    
-    fprintf(stderr, "\nbeforemethodcall:foo\n");
     foo(a, b);
-    fprintf(stderr, "\naftermethodcall:foo\n");
-    
 
     return 0;
 }
